5279f9d3273987cc2fce260c3b096523b8213e23
alexfankhauser <alexfankhauser@ALEXFANKHAU8EDB>
2024-01-17 09:31:07 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// Added this so that I can call the Roll() method
+using DiceSimulator;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Create a variable for the number of rolls and for the output array
+        int numRolls;
+        int[] output;
+
+        // Create an instance of the RollDice class
+        RollDice rd = new RollDice();
+
+        // Write the introduction and ask for the number of dice rolls
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n\n" +
+            "We are glad you are trying your luck here rather than in Vegas.\n\n" +
+            "How many dice throws do you want to simulate?");
+
+        // Receive input for the number of rolls and make it an integer
+        numRolls = int.Parse(System.Console.ReadLine());
+
+        // Make the output array the array of results returned from the rd.Roll method
+        output = rd.Roll(numRolls);
+
+        // Write the introduction to the results
+        System.Console.WriteLine("\nDICE THROWING SIMULATOR RESULTS\n" +
+            "Each '*' represents 1% of the total number of rolls\n" +
+            "Total number of rolls is " + numRolls);
+
+        // Call the PrintResults method to make the histogram
+        PrintResults(output, numRolls);
+    }
+
+    // This is my PrintResults method that receives the results array and the number of rolls
+    private static void PrintResults(int[] results, int numRolls)
+    {
+        // Create a loop for each of the possible sums of the two dice rolls
+        for (int i = 2; i <= 12; i++)
+        {
+            // Create a percentage of rolls for each increment
+            int percent = (results[i - 2] * 100) / numRolls;
+            // Create a string of * based on the percent
+            string asterisks = new string('*', percent);
+            // Write the line of the number and its asterisks
+            System.Console.WriteLine(i + ": " + asterisks);
+        }
+
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceSimulator
+{
+    internal class RollDice
+    {
+        // Here is the Roll method that receives a number of rolls from the program
+        // It will return an array of integers
+        public int[] Roll(int numRolls)
+        {
+            // use random
+            Random random = new Random();
+
+            // Create a new array of results that has a length of 11
+            int[] results = new int[11];
+
+            // Create a loop to roll two dice numRolls times
+            for (int i = 0; i < numRolls; i++)
+            {
+                // Roll each dice (random number between 1 and 7)
+                int dice1 = random.Next(1, 7);
+
+                int dice2 = random.Next(1, 7);
+
+                // Sum the two rolls together
+                int sum = dice1 + dice2;
+
+                // Add the results to the array
+                results[sum - 2]++;
+            }
+
+            // Return the array of results
+            return results;
+
+        }
+    }
+}
